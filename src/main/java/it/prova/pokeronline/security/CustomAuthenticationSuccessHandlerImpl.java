a182e52a0fc44ba8990779d172154f066dd373fa
Vincenzo Pipitone <82387646+pipitonedev@users.noreply.github.com>
2021-11-13 13:07:05 +0100

    Creazione Security

---
+++ src/main/java/it/prova/pokeronline/security/CustomAuthenticationSuccessHandlerImpl.java
@@ -0,0 +1,40 @@
+package it.prova.pokeronline.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import it.prova.pokeronline.model.Utente;
+import it.prova.pokeronline.repository.utente.UtenteRepository;
+
+@Component
+public class CustomAuthenticationSuccessHandlerImpl implements AuthenticationSuccessHandler {
+
+	@Autowired
+	private UtenteRepository utenteRepository;
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+
+		// voglio mettere in sessione uno userInfo perché spring security mette solo un
+		// principal da cui attingere username
+		Utente utenteFromDb = utenteRepository.findByUsername(authentication.getName()).orElseThrow(
+				() -> new UsernameNotFoundException("Username " + authentication.getName() + " not found"));
+		Utente utenteParziale = new Utente();
+		utenteParziale.setNome(utenteFromDb.getNome());
+		utenteParziale.setCognome(utenteFromDb.getCognome());
+		request.getSession().setAttribute("userInfo", utenteParziale);
+		response.sendRedirect("home");
+
+	}
+
+}
---
+++ src/main/java/it/prova/pokeronline/security/SecSecurityConfig.java
@@ -0,0 +1,65 @@
+package it.prova.pokeronline.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
+	
+	@Autowired
+    private CustomAuthenticationSuccessHandlerImpl successHandler;
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+ 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+         .userDetailsService(customUserDetailsService)
+         .passwordEncoder(passwordEncoder());
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	 http.authorizeRequests()
+         .antMatchers("/assets/**").permitAll()
+         .antMatchers("/login").permitAll()
+         .antMatchers("/utente/cambioPassword").hasAnyRole("ADMIN", "CLASSIC_USER")
+         .antMatchers("/utente/saveCambioPassword").hasAnyRole("ADMIN", "CLASSIC_USER")
+         .antMatchers("/utente/**").hasRole("ADMIN")
+         .antMatchers("/**").hasAnyRole("ADMIN", "CLASSIC_USER")
+         //.antMatchers("/anonymous*").anonymous()
+         .anyRequest().authenticated()
+         .and().exceptionHandling().accessDeniedPage("/accessDenied")
+         .and()
+         	.formLogin()
+         	.loginPage("/login")
+         	//.defaultSuccessUrl("/home",true)
+         	//uso un custom handler perché voglio mettere delle user info in session
+         	.successHandler(successHandler)
+         	.failureUrl("/login?error=true")
+         	.permitAll()
+         .and()
+         	.logout()
+         	.logoutSuccessUrl("/executeLogout")
+            .invalidateHttpSession(true)
+            .permitAll()
+         .and()
+            .csrf()
+            .disable();
+//         
+    }
+}
